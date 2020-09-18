3f8887137fcedae0ad50a578f40f0d9e9612dcc5
Lazar Mihailovic <lazar.mihailovic@spottedzebra.co.uk>
2020-09-18 18:08:36 +0200

    add: type-scores component

---
+++ components/molecules/TypeScores/TypeScores.tsx
@@ -0,0 +1,21 @@
+import './TypeScores.scss';
+import React, { FC } from 'react';
+import Type, { IType } from '../../atoms/Type/Type';
+
+interface ITypeScores {
+  typeScores: IType[];
+}
+
+const TypeScores: FC<ITypeScores> = props => {
+  const { typeScores } = props;
+
+  return (
+    <div className="TypeScores">
+      {typeScores.map(type => {
+        return <Type key={type.name} name={type.name} grade={type.grade} />
+      })}
+    </div>
+  );
+};
+
+export default TypeScores;
