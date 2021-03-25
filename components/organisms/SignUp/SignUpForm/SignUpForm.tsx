b24b4229a28d8afdd266f2e2e7799dc904eb516c
mljubic-biz <marko.ljubic@spottedzebra.co.uk>
2021-03-25 12:14:01 +0100

    feature: remove redundant wiser applied from field

--- components/organisms/SignUp/SignUpForm/SignUpForm.tsx
+++ components/organisms/SignUp/SignUpForm/SignUpForm.tsx
@@ -4,7 +4,6 @@ import validate from '../../../../helpers/validate';
 import { TFormFieldValue } from '../../../../interfaces/form';
 import FormField from '../../../atoms/FormField/FormField';
 import Input from '../../../atoms/Input/Input';
-import Select from '../../../atoms/Select/Select';
 import PasswordInput from '../../../molecules/PasswordInput/PasswordInput';
 import SquareButton from '../../../molecules/SquareButton/SquareButton';
 import PrivacyPolicyCheckboxField from './PrivacyPolicyCheckboxField/PrivacyPolicyCheckboxField';
@@ -13,7 +12,6 @@ interface ISignUpFormValues {
   fullName: string;
   email: string;
   password: string;
-  appliedFrom: string;
   isPrivacyPolicyChecked: boolean;
 }
 
@@ -21,48 +19,25 @@ interface ISignUpFormErrors {
   fullName: string[];
   email: string[];
   password: string[];
-  appliedFrom: string[];
   isPrivacyPolicyChecked: string[];
 }
 
 interface ISignUpForm {
   fullName?: string;
   email?: string;
-  hasAppliedFromField?: boolean;
   onSignUp: (
     fullName: string,
     email: string,
     password: string,
-    appliedFrom: string,
     isPrivacyPolicyChecked: boolean
   ) => void;
 }
-
-const appliedFromOptions = [
-  {
-    label: 'Audit',
-    value: 'audit',
-  },
-  {
-    label: 'Tax',
-    value: 'tax',
-  },
-  {
-    label: 'Business Support',
-    value: 'business support',
-  },
-  {
-    label: 'School leavers',
-    value: 'school leavers',
-  },
-];
 
 const SignUpForm: FC<ISignUpForm> = props => {
   const [values, setValues] = useState<ISignUpFormValues>({
     fullName: props.fullName || '',
     email: props.email || '',
     password: '',
-    appliedFrom: '',
     isPrivacyPolicyChecked: false,
   });
   const [errors, setErrors] = useState<ISignUpFormErrors | undefined>();
@@ -86,14 +61,6 @@ const SignUpForm: FC<ISignUpForm> = props => {
       password: {
         length: { minimum: 5 },
       },
-      appliedFrom: props.hasAppliedFromField
-        ? {
-            length: {
-              minimum: 1,
-              tooShort: '^Applied from must be selected.',
-            },
-          }
-        : {},
       isPrivacyPolicyChecked: {
         exclusion: {
           within: { false: false },
@@ -120,7 +87,6 @@ const SignUpForm: FC<ISignUpForm> = props => {
         values.fullName,
         values.email,
         values.password,
-        values.appliedFrom,
         values.isPrivacyPolicyChecked
       );
     }
@@ -171,22 +137,6 @@ const SignUpForm: FC<ISignUpForm> = props => {
             value={values.password}
           />
         </FormField>
-        {props.hasAppliedFromField && (
-          <FormField
-            error={errors && errors.appliedFrom && errors.appliedFrom.join(' ')}
-            fieldName="appliedFrom"
-            label="Stream applied for"
-            isLabelVisible={!!values.appliedFrom}
-          >
-            <Select
-              name="appliedFrom"
-              id="sign-up-applied-from"
-              value={values.appliedFrom}
-              options={appliedFromOptions}
-              onChange={handleChange}
-            />
-          </FormField>
-        )}
       </div>
       <div className="SignUpForm__Bottom">
         <PrivacyPolicyCheckboxField
