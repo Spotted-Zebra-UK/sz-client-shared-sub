fd5c19d521a4b0fa7d107ebc10ad3e88123ebb7d
marko.ljubic <marko.ljubic@spottedzebra.co.uk>
2021-06-01 10:17:08 +0200

    wip

--- components/atoms/Bubble/Bubble.tsx
+++ components/atoms/Bubble/Bubble.tsx
@@ -1,10 +1,12 @@
 import './Bubble.scss';
 import React, { FC } from 'react';
 
-export interface IBubble {}
+export interface IBubble {
+  className?: string;
+}
 
-const Bubble: FC<IBubble> = () => {
-  return <div className="Bubble" />;
+const Bubble: FC<IBubble> = ({ className }) => {
+  return <div className={`Bubble${className ? ` ${className}` : ''}`} />;
 };
 
 export default Bubble;
