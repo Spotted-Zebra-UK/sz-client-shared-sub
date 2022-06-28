893c81a3fef4d3d2d17e5bcf40e760ad2ece2b74
antibioticvz@gmail.com <antibioticvz@gmail.com>
2022-06-28 04:58:13 -0500

    add date const

--- constants/dateFormats.ts
+++ constants/dateFormats.ts
@@ -1,3 +1,4 @@
 export const DATE_FORMAT_DATE = 'YYYY-MM-DD';
+export const DATE_FORMAT_DATE_TABLE = 'DD-MM-YYYY';
 export const DATE_FORMAT_DATE_TIME = 'YYYY-MM-DD hh:mm:ss';
 export const API_DATE_FORMAT_DATE = 'DD/MM/YYYY';
